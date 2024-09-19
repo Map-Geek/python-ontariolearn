6fd084d15c62a37fa91cd6213dcb2414d92219f7
Map-Geek <geetha.ramesh@dcmail.ca>
2024-09-19 14:36:19 -0700

    first file

---
+++ hello.py
@@ -0,0 +1,16 @@
+"""
+The Profile Program
+
+This is a program that prints the name, occupation and a short Biography.
+To use it, run 'python hello.py'.
+"""
+
+# Print Name - First Name
+# Print Occupation - job
+print("Name: Geetha")
+print("Occupation: Student")  # Displaying Occupation of the Person
+
+# Prints Biography of a Person
+print("Biography: I am a student of Durham College,Ontario. I love learning new things.\n"
+      "Recently I learnt a mapping course which was very interesting.\n"
+      "I also like going on hikes, cycling, gardening and traveling.")
